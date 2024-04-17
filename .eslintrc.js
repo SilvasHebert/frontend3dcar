7480613296892f217cd6d061053bea7d18ad7b63
hebert.silva@cibus.app.br <hebert.silva@cibus.app.br>
2024-04-17 01:19:41 -0300

    refactor: adicionado sort import ao eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,28 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  rules: {'linebreak-style': 0},
+  plugins: ['react', 'simple-import-sort'],
+  rules: {
+    'linebreak-style': 0,
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Packages `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Internal packages.
+          ['^(@|components)(/.*|$)'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.?(css)$'],
+        ],
+      },
+    ],
+  },
 };
