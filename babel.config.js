3feb589de09da859acec3cbc6c35ed1e52b84974
hebert.silva@cibus.app.br <hebert.silva@cibus.app.br>
2024-04-20 19:56:37 -0300

    style: definido custom import paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@app': './app',
+          '@consts': './app/consts',
+          '@components': './app/components',
+          '@assets': './app/assets',
+        },
+      },
+    ],
+  ],
 };
--- ios/frontend3dcar/AppDelegate.mm
+++ ios/frontend3dcar/AppDelegate.mm
@@ -2,29 +2,35 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  self.moduleName = @"frontend3dcar";
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+  [GMSServices provideAPIKey:@"AIzaSyAF5D9-gErY_58KHPqfhiEMX2Zjeh4bDZw"]
+
+      self.moduleName = @"frontend3dcar";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  return [super application:application
+      didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
   return [self getBundleURL];
 }
 
-- (NSURL *)getBundleURL
-{
+- (NSURL *)getBundleURL {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  return
+      [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [[NSBundle mainBundle] URLForResource:@"main"
+                                 withExtension:@"jsbundle"];
 #endif
 }
 
